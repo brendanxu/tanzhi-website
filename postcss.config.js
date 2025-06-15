152d15762e8809539fbc5407f1a4ed0dbb8c0e50
Xu Hui <xuhui413@aliyun.com>
2025-06-15 17:20:40 +0800

    chore: convert postcss.config.js to CommonJS format

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,9 @@
 // postcss.config.js
-
-/** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
-    '@tailwindcss/postcss': {}, // <-- 这里的单引号是关键！
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
-
-export default config
