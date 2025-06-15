85d2551e6211fa9ac940dc12918bfeece185f2ee
Xu Hui <xuhui413@aliyun.com>
2025-06-15 17:16:14 +0800

    fix(postcss): finally correct syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+// postcss.config.js
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    @tailwindcss/postcss: {},
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    '@tailwindcss/postcss': {}, // <-- 这里的单引号是关键！
     autoprefixer: {},
   },
 }
+
+export default config
